7c6d93e41daa09c66c760ce258146ee71b6cd5a0
fc53293 <gongas.figueiredo09@gmail.com>
2021-03-21 00:45:42 +0000

    Add file

---
+++ app/Http/Controllers/apiGatewayController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+use DB;
+use App\routes\web;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Artigo;
+
+class apiGatewayController extends Controller
+{
+    public function showSigninPage()
+    {
+        return view('login');
+    }
+
+    public function showSignupPage()
+    {
+        return view('register');
+    }
+
+    public function allArtigos()
+    {
+        $inquilino = Artigo::all();
+        //$inquilino = $this->model->all();
+
+        return response()->json($inquilino);
+        
+        //return response()->json('Mostra todas os inquilinos');
+        
+    }
+
+    public function createArtigo(Request $request)
+    {
+        print_r($request->input());
+        $order = new Artigo;
+        $order->email=$request->input('email');
+        $order->id=$request->input('description');
+        $order->save();
+       //$inquilino = Inquilino::create($request->all());
+
+       //return response()->json($inquilino);
+    }
+
+
+    public function mostraArtigos()
+    {
+        $results = DB::select("SELECT * FROM artigos");
+        return response()->json($results);
+    }
+}
+
+
+?>
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+//use Illuminate\Support\Facades\Route;
+
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+
+$router->group(['prefix' => 'api'], function($router)
+{
+    $router->post('artigo/add','apiGatewayController@createArtigo');
+    $router->get('artigo/all','apiGatewayController@allArtigos');
+    
+}); 
+
+Route::get('/signin', 'apiGatewayController@showSigninPage');
+
+Route::get('/signup', 'apiGatewayController@showSignupPage');
+
+Route::get('signin2', 'apiGatewayController@mostraArtigos');
