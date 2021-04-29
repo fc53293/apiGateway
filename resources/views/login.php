38227e01e1512e3110e01279f140b71c43b0c55e
Jorge Roque <jorgeroque112@gmail.com>
2021-04-29 23:04:07 +0100

    Alteracoes Login

--- resources/views/login.php
+++ resources/views/login.php
@@ -64,20 +64,22 @@
              <button type="submit" class="m-5 btn btn-primary btn-lg btn-block">Sign in</button> 
           </div>
             <?php 
-                $ch = curl_init("http://microinquilino-deployment:8081/");
-                curl_exec($ch);
+                
+                //curl_exec($ch);
+
+                $defaults = array(
+                  CURLOPT_URL => 'http://microinquilino-deployment:8081/',
+                  CURLOPT_POST => true
+                );
+
+                $ch = curl_init();
+
+                curl_setopt_array($ch, $defaults);
+
+                //curl_exec($ch)
+                
             ?>
           
-            <a href="http://microinquilino.default.svc.apigateway-cluster:8081/inquilino"> Quero ir para aqui  1 </a> <br>
-
-            <a href="http://microinquilino.default.svc.apigateway-cluster:8081"> Quero ir para aqui  2 </a> <br>
-
-            <a href="http://microinquilino-deployment.default.svc.apigateway-cluster:8081/inquilino"> Quero ir para aqui  3 </a><br>
-
-            <a href="http://microinquilino-deployment.default.svc.apigateway-cluster:8081"> Quero ir para aqui 4 </a><br>
-            
-
-            
       </form>
     </div>
 
